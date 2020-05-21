a374fa915b464993435e8ed06299b6ea272b1a56
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-05-21 23:57:36 +0530

    Added Daily Wage Calculator

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,3 +1,4 @@
+// use case 1 --- Branch
 public class EmpWageBuilder {
 
 	public static void main(String[] args) {
@@ -8,6 +9,26 @@ public class EmpWageBuilder {
 		else	{
 			System.out.println("Employee is ABSENT");
 		}
+
+	//UC2 new Branch--
+	int Wage_per_hr=20;
+	int Full_Day_hr=8;
+
+	int Daily_Wage=(is_Present * Wage_per_hr * Full_Day_hr);
+
+	System.out.println("Daily Wage = "+Daily_Wage);
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
 
